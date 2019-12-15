d7e13dfbffae2cbd7ab87e51631d593e6d9c29a8
permasu <32789955+permasu@users.noreply.github.com>
2019-12-15 22:49:48 +0500

    Update Laba5.py

--- Laba5.py
+++ Laba5.py
@@ -1,36 +1,55 @@
 # -*- coding: utf-8 -*-
 import unittest
+
+
 class journal:
     def __init__(self, name, faculty):
         self.name = name
         self.faculty = faculty
+
     def __repr__(self):
         return "%s, %r" % (self.name, self.faculty)
+
     def __str__(self):
         return "ФИО: %s, группа: %r" % (self.name, self.faculty)
+
     def test(self):
         return str(self)
+
     def __call__(self, *args):
-        return  print("был вызван CALL:%s , %r" % (self.name,self.faculty))
-class testjournal(unittest.TestCase):
-    def testInsufficientArgs(self):
+        return print("был вызван CALL:%s , %r" % (self.name, self.faculty))
 
-my_list = [
-    journal('Ахметшин Сергей', 'АСУ-19-1'),
-    journal('Рожин Павел', 'АСУ-19-1')
-]
-print('Демонстрация lambda')
-printjournal=lambda x: print("ФИО: %s, группа: %r" % (x.name, x.faculty))
-for x in my_list:
-    printjournal(x)
-new_list= list(map(lambda x: x.name+" map",my_list))
+    def __lt__(self, other):
+        return self.name < other.name
+    def __eq__(self, other):
+        return self.name == other.name and self.faculty == other.faculty
 
-print("Демонстрация map")
-print(new_list)
-print("Демонстрация reduce// Суммирование фамилий в списке")
-from functools import reduce
-sum_all_Fio=reduce(lambda x, y: x.name+" "+y.name, my_list)
-print(sum_all_Fio)
-print("Демонстрация filter//отфильтруем элементы с имеющимся вхождением строки Ахметшин Сергей в фио")
-filt = list(filter(lambda x: x.name=='Ахметшин Сергей', my_list))
-print(filt)
+
+def SortJournal(slist):
+    slisttemp=slist
+    for i in range(len(slisttemp) - 1):
+        for j in range(len(slisttemp) - i - 1):
+            if (slisttemp[j + 1] < slisttemp[j]):
+                slisttemp[j], slisttemp[j + 1] = slisttemp[j + 1], slisttemp[j]
+    return slisttemp
+
+
+class SortMyListTest(unittest.TestCase):
+    def test_normal(self):
+        res = SortJournal([journal('Ахметшин Сергей', 'АСУ-19-1'),
+                            journal('Шарифханов Артем', 'АСУ-19-1'),
+                            journal('Трошева Кристина', 'АСУ-19-1'),
+                            journal('Рожин Павел', 'АСУ-19-1')])
+        self.assertEqual(res, [journal('Ахметшин Сергей', 'АСУ-19-1'),
+                               journal('Рожин Павел', 'АСУ-19-1'),
+                               journal('Трошева Кристина', 'АСУ-19-1'),
+                               journal('Шарифханов Артем', 'АСУ-19-1')])
+
+    def test_single(self):
+        res = SortJournal([journal('Ахметшин Сергей', 'АСУ-19-1')])
+        self.assertEqual(res, [journal('Ахметшин Сергей', 'АСУ-19-1')])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
